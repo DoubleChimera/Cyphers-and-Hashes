d8ceebf9d0212ce6f0de5026707fb42a4bb02193
Cake <52736573+DoubleChimera@users.noreply.github.com>
2019-07-20 22:04:42 -0700

    setup CryptoPals S1C3

---
+++ CryptoPals/Set 1/Challenge_3.py
@@ -0,0 +1,21 @@
+
+# CryptoPals  -  Set 1  -  Challenge 3
+
+### ! NOTES:    
+
+# Imports
+import codecs
+#Given values
+outputCheck = ''
+### Code the solution
+
+
+
+###  Check the result
+print(' Expected Output: {}'.format(outputCheck))
+print('Generated Output: {}'.format())
+
+if (outputCheck == b64):
+    print('\nSUCCESS!')
+else:
+    print('\nFAILURE...')
